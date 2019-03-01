6a658530aa23d91b7e519239b09cde990a6419bc
Kennyproz <k.odigwegomez@student.fontys.nl>
2019-03-01 11:26:34 +0100

    Fixed unit test

--- src/test/java/models/RoleTest.java
+++ src/test/java/models/RoleTest.java
@@ -5,6 +5,8 @@ import dal.contexts.memory.RoleMemoryDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.management.relation.RoleNotFoundException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoleTest {
@@ -23,7 +25,13 @@ class RoleTest {
         assertEquals(0,roleMemoryContext.roles().size());
         assertEquals(role,roleMemoryContext.add(role));
         assertEquals(1,roleMemoryContext.roles().size());
-        assertEquals(role,roleMemoryContext.add(role));
+        try {
+            roleMemoryContext.add(role);
+        } catch (RoleExistsException e) {
+        }
+        assertThrows(RoleExistsException.class, () -> {
+            roleMemoryContext.add(role);
+        });
         assertEquals(1,roleMemoryContext.roles().size());
     }
 
